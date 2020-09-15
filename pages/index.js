b121dda2b3a1a79579aeed040957779bd6d94983
Apple <apple@ip-192-168-1-102.ap-south-1.compute.internal>
2020-09-16 02:48:04 +0530

    issue fixed

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ const landingStatus = ["True", "False"];
 export default function Home({ launches }) {
   const [launchesArray, setLaunchesArray] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [year_st, setYear_st] = useState(null);
+  const [launch_st, setLaunch_st] = useState(null);
+  const [landing_st, setLanding_st] = useState(null);
 
   useEffect(() => {
     setLaunchesArray(launches);
@@ -43,17 +46,38 @@ export default function Home({ launches }) {
   }
 
   const yearFilter = async (year) => {
-    let launches = await getData(year);
+    if (year == year_st) {
+      setYear_st(null);
+      year = null;
+    }
+    else {
+      setYear_st(year);
+    }
+    let launches = await getData(year, launch_st, landing_st);
     setLaunchesArray(launches);
   }
 
   const launchFilter = async (launch) => {
-    let launches = await getData(null, launch);
+    if (launch == launch_st) {
+      setLaunch_st(null);
+      launch = null;
+    }
+    else {
+      setLaunch_st(launch);
+    }
+    let launches = await getData(year_st, launch, landing_st);
     setLaunchesArray(launches);
   }
 
   const landingFilter = async (landing) => {
-    let launches = await getData(null, null, landing);
+    if (landing == landing_st) {
+      setLanding_st(null);
+      landing = null;
+    }
+    else {
+      setLanding_st(landing);
+    }
+    let launches = await getData(year_st, launch_st, landing);
     setLaunchesArray(launches);
   }
 
@@ -69,7 +93,7 @@ export default function Home({ launches }) {
           <h2 style={{ margin: 0 }}>SpaceX Launch Programs</h2>
         </Row>
         <Row>
-          <Col xs={12} sm={12} md={2} lg={2} style={{ backgroundColor: 'white', padding: 10, marginTop: 10 }}>
+          <Col xs={12} sm={4} md={4} lg={2} style={{ backgroundColor: 'white', padding: 10, marginTop: 10, height: '100%' }}>
             <h3>
               <b>Filters</b>
             </h3>
@@ -78,7 +102,9 @@ export default function Home({ launches }) {
               {years.map((year, index) => {
                 return (
                   <Col xs={6} sm={6} md={6} lg={6} key={index} style={{ padding: "5px 10px" }}>
-                    <button className={styles.button} onClick={() => { yearFilter(year) }}>{year}</button>
+                    <button className={styles.button}
+                      style={year == year_st ? { backgroundColor: '#37ca57' } : {}}
+                      onClick={() => { yearFilter(year) }}>{year}</button>
                   </Col>
                 )
               })}
@@ -88,7 +114,9 @@ export default function Home({ launches }) {
               {launchStatus.map((launch, index) => {
                 return (
                   <Col xs={6} sm={6} md={6} lg={6} key={index} style={{ padding: "5px 10px" }}>
-                    <button className={styles.button} onClick={() => { launchFilter(launch) }}>{launch}</button>
+                    <button className={styles.button}
+                      style={launch == launch_st ? { backgroundColor: '#37ca57' } : {}}
+                      onClick={() => { launchFilter(launch) }}>{launch}</button>
                   </Col>
                 )
               })}
@@ -98,19 +126,22 @@ export default function Home({ launches }) {
               {landingStatus.map((landing, index) => {
                 return (
                   <Col xs={6} sm={6} md={6} lg={6} key={index} style={{ padding: "5px 10px" }}>
-                    <button className={styles.button} onClick={() => { landingFilter(landing) }}>{landing}</button>
+                    <button className={styles.button}
+                      style={landing == landing_st ? { backgroundColor: '#37ca57' } : {}}
+                      onClick={() => { landingFilter(landing) }}>{landing}</button>
                   </Col>
                 )
               })}
             </Row>
           </Col>
-          <Col xs={12} sm={12} md={10} lg={10}>
+          <Col xs={12} sm={8} md={8} lg={10}>
             {loading && <p>loading...</p>}
             {!loading && <Row>
-              {launchesArray.map((item, index) => {
+              {launchesArray.length == 0 && <h3 style={{marginLeft:10}}>No Result Found</h3>}
+              {launchesArray.length != 0 &&launchesArray.map((item, index) => {
                 return (
-                  <Col key={index} xs={12} sm={6} md={4} lg={3} style={{ padding: 10 }}>
-                    <div className="card" style={{ padding: 10, backgroundColor: 'white' }}>
+                  <Col key={index} xs={12} sm={6} md={6} lg={3} style={{ padding: 10 }}>
+                    <div className="card" style={{ padding: 10, backgroundColor: 'white', height: '100%' }}>
                       <div className="imageContainer" style={{ width: '100%', backgroundColor: 'whitesmoke', padding: 5 }}>
                         <img style={{ width: '100%' }} src={item.links.mission_patch_small}></img>
                       </div>
